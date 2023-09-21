8a521334f0604e2203df037eb308202f78eda0a4
Sander van Dijk <sander.vandijk@streetdrone.com>
2023-09-21 11:09:54 +0200

    Do not wait for approximate position before continueing

--- oxts_driver/include/oxts_driver/driver.hpp
+++ oxts_driver/include/oxts_driver/driver.hpp
@@ -186,24 +186,18 @@ public:
         (*this.*update_ncom)();
       }
       RCLCPP_INFO(this->get_logger(), "INS initialised");
-    } else {
-      RCLCPP_INFO(this->get_logger(), "Waiting for approximate position...");
-      // Wait for a approximate position (while uninitialised, orientation is
-      // never approx/valid)
-      while (nrx->mIsLatApprox == 0 && nrx->mIsLonApprox == 0 &&
-             nrx->mIsAltApprox == 0 && nrx->mIsLatValid == 0 &&
-             nrx->mIsLonValid == 0 && nrx->mIsAltValid == 0) {
-        (*this.*update_ncom)();
-      }
-      RCLCPP_INFO(this->get_logger(),
-                  "Publishing before/without INS initialisation");
-    }
-
-    timer_ncom_ = this->create_wall_timer(ncomInterval,
-                                          std::bind(timer_ncom_callback, this));
-
-    RCLCPP_INFO(this->get_logger(), "Publishing NCom packets at: %uHz",
-                ncom_rate);
+    }  //  else {
+    //   RCLCPP_INFO(this->get_logger(), "Waiting for approximate position...");
+    //   // Wait for a approximate position (while uninitialised, orientation is
+    //   // never approx/valid)
+    //   while (nrx->mIsLatApprox == 0 && nrx->mIsLonApprox == 0 &&
+    //          nrx->mIsAltApprox == 0 && nrx->mIsLatValid == 0 &&
+    //          nrx->mIsLonValid == 0 && nrx->mIsAltValid == 0) {
+    //     (*this.*update_ncom)();
+    //   }
+    //   RCLCPP_INFO(this->get_logger(),
+    //               "Publishing before/without INS initialisation");
+    // }
   }
 
   /** NCom decoder instance */
